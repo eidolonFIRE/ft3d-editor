834f8ff79bb5f057f834ce5893ef57c3cd4662c7
Randy Ransier <randy.ransier@icloud.com>
2021-12-12 20:14:14 -0500

    csv: change format to match Repeater Book.
    Update the CSV format to match Repeater Book's Yaesu FT3 ADMS 11
    export format. It seems this format might have been added to Repeater
    Book after this tool was developed, but I'm not exactly sure on that.
    Anyhow, the goal is essentially to be able to export from Repeater Book
    and then use this tool to create a MEMORY.dat that can be loaded onto
    the radio via microSD. There are more fields and they are in a different
    order than the format this tool was using, but one of the benefits is
    that we can capture more of the data fields so that we can import and
    export without too much being changed the process.
    
    Repeater Book documents their Yaesu FT3 ADMS 11 CSV fields at
    https://www.repeaterbook.com/wiki/doku.php?id=yaesu_adms_11.

--- src/channel.py
+++ src/channel.py
@@ -31,11 +31,12 @@ class Channel():
         self.clock_shift = ClockShift((struct.optionsA >> 4) & 0x1)
         self.bandw = Bandw((struct.optionsA >> 5) & 0x1)
         self.s_meter = struct.s_meter & 0xf
-        self.squelch = struct.s_meter >> 7
+        self.squelch = bool(struct.s_meter >> 7)
         self.offset = self._parse_freq(struct.offset)
         self.charset = struct.charset
         self.banks = []
         self.rx_mode = RxMode.AM if (struct.step >> 6) & 0x1 else (RxMode.AUTO if ((struct.misc_options >> 3) & 0x1) else RxMode.FM)
+        self.priority = False
 
         if banks is not None:
             for each in banks:
@@ -79,7 +80,7 @@ class Channel():
         chan.tone = TONES.index(self.tone)
         chan.dcs = DCS_List.index(self.dcs)
         chan.dcs_pol = self.dcs_pol << 4
-        chan.s_meter = self.s_meter | (self.squelch << 7)
+        chan.s_meter = self.s_meter | ((1 if self.squelch else 0) << 7)
         chan.misc_options = self.bell | self.attn << 5 | ((self.rx_mode == RxMode.AUTO) << 3) | (self.unk_misc << 4)
         if self.charset is not None:
             chan.charset = self.charset
@@ -94,61 +95,102 @@ class Channel():
         self.raw = None
         self.empty = False
         self.index = int(csv[0])
-        self.skip = csv[1] == "S"
-        self.enabled = csv[1] != "R"
-        self.name = csv[2]
+        self.priority = csv[1] == "ON"
+        self.skip = csv[19] == "ON"
+        self.step = Step[f"_{csv[21].strip('KHz').replace('.','_')}" if csv[21] else "_auto"]
+        self.attn = csv[23] == "ON"
+        self.bell = csv[25] == "ON"
+        self.clock_shift = ClockShift.A if csv[27] == "OFF" else ClockShift.B
+        self.name = csv[10]
         self.charset = (c_ubyte * 2)(*[0x00, 0x00])
-        for x in re.finditer("(#[a-zA-Z])", csv[2]):
+        for x in re.finditer("(#[a-zA-Z])", csv[10]):
             i = x.span()[0]
             self.charset[i // 8] |= 0x1 << (7 - i%8)
 
-        self.freq = round(float(csv[3]), 3)
-        self.bandw = Bandw.Narrow if "N" in csv[4] else Bandw.Wide
-        self.s_meter = 0
-        self.txpwr = TxPwr[csv[5]] if csv[5] else TxPwr.High
-        if csv[6]:
-            self.offset = abs(round(float(csv[6]), 3))
-            self.offset_pol = (OffsetPol.MINUS if float(csv[6]) < 0 else OffsetPol.PLUS) if float(csv[6]) != 0.0 else OffsetPol.NONE
+        self.freq = round(float(csv[2]), 3)
+        self.bandw = Bandw.Narrow if csv[26] == "ON" else Bandw.Wide
+        self.squelch = csv[24] == "ON"
+        self.txpwr = TxPwr[csv[18].split()[0]] if csv[18].startswith("L") else TxPwr.High
+        self.offset = abs(round(float(csv[4]), 3)) if csv[4] else 0.0
+        if csv[5] == "OFF":
+            self.offset_pol = OffsetPol.OFF
+        elif csv[5] == "+/-":
+            self.offset_pol = OffsetPol.PLUS_MINUS
+        elif csv[5].startswith("-"):
+            self.offset_pol = OffsetPol.MINUS
         else:
-            self.offset = 0.0
-            self.offset_pol = OffsetPol.NONE
+            self.offset_pol = OffsetPol.PLUS
 
         # airband special case
         if 108.0 <= self.freq <= 137 or csv[7].upper() == "AM":
             self.mode = Mode.FM
             self.rx_mode = RxMode.AM
         else:
-            self.mode = Mode[csv[7]]
-            self.rx_mode = RxMode.AUTO
+            self.mode = Mode[csv[8]]
+            self.rx_mode = RxMode.AUTO if csv[6] == "ON" else RxMode.FM
 
-        self.tone_mode = ToneMode[csv[8] or "NONE"]
-        self.squelch = 0 if self.tone_mode > 0 else 1
-        self.tone = float(csv[9]) if csv[9] else 100.0
-        self.dcs = int(csv[10]) if csv[10] else DCS_List[0]
-        self.dcs_pol = DCSPolarity[csv[11]] if csv[11] else DCSPolarity(0)
-        self.banks = [i for i, x in enumerate(csv[12:36]) if x.strip().upper() == "TRUE"]
+        self.tone_mode = ToneMode[csv[11].replace(' ', '_') or "OFF"]
+        self.tone = float(csv[12].split()[0]) if csv[12] else 100.0
+        self.dcs = int(csv[13]) if csv[13] else DCS_List[0]
+        self.dcs_pol = DCSPolarity[csv[14].replace(' ', '_')] if csv[14] else DCSPolarity(0)
+        self.banks = [i for i, x in enumerate(csv[28:52]) if x.strip().upper() == "ON"]
 
-        self.bell = 0
-        self.attn = 0
+        self.enabled = True
+        self.s_meter = 0
         self.unk_misc = 1
-        self.clock_shift = 0
-        self.step = Step(0)
 
     def to_csv(self):
-        return "{idx:4}, {flag:1}, {name:16}, {rxfq:6.3f}, {bndw:1}, {pwr}, {ofst:4.1f}, {mode:>3}, {tnmd:3}, {tone}, {dcs:3}, {dcs_pol:7},{banks},".format(
-            idx =self.index,
-            flag=self._channel_state(),
-            name=self.name[:16],
+        if self.offset_pol == OffsetPol.OFF:
+            txfq = self.freq
+            ofdr = "OFF"
+        elif self.offset_pol == OffsetPol.PLUS_MINUS:
+            txfq = 0.0
+            ofdr = "+/-"
+        elif self.offset_pol == OffsetPol.MINUS:
+            txfq = self.freq - self.offset
+            ofdr = "-RPT"
+        else:
+            txfq = self.freq + self.offset
+            ofdr = "+RPT"
+        if self.txpwr == TxPwr.L1:
+            pwr = "L1 (0.1W)"
+        elif self.txpwr == TxPwr.L2:
+            pwr = "L2 (1W)"
+        elif self.txpwr == TxPwr.L3:
+            pwr = "L3 (2.5W)"
+        else:
+            pwr = "High (5W)"
+        return "{idx:4},{priority},{rxfq:6.3f},{txfq:6.3f},{ofst:4.1f},{ofdr},{auto},{opmode},{mode},{tag},{name:16},{tnmd},{tone},{dcs:3},{dcs_pol},{usrtone},{rxdgid},{txdgid},{pwr},{skip},{autostep},{step},{mask},{attn},{smeter_sql},{bell},{bndw:1},{clock_shift},{banks},{comment},0".format(
+            idx=self.index,
+            priority="ON" if self.priority else "OFF",
             rxfq=self.freq,
-            bndw="N" if self.bandw is Bandw.Narrow else "",
-            ofst=(self.offset * (-1 if self.offset_pol is OffsetPol.MINUS else 1)) if self.offset_pol > 0 else 0.0,
-            pwr =self.txpwr.name,
-            mode=RxMode.AM.name if self.rx_mode is RxMode.AM else self.mode.name,
-            tnmd=self.tone_mode.name if self.tone_mode else "",
-            tone="{:5.1f}".format(self.tone) if self.tone_mode else "",
-            dcs =self.dcs if self.tone_mode is ToneMode.DCS else "",
-            dcs_pol=self.dcs_pol.name if self.tone_mode is ToneMode.DCS else "",
-            banks=",".join([" TRUE" if x else "FALSE" for x in self.banks])
+            txfq=txfq,
+            ofst=self.offset,
+            ofdr=ofdr,
+            auto="ON" if self.rx_mode == RxMode.AUTO else "OFF",
+            opmode="AM" if self.rx_mode == RxMode.AM else "FM",
+            mode=self.mode.name,
+            tag="ON",
+            name=self.name[:16],
+            tnmd=self.tone_mode.name.replace('_', ' '),
+            tone="{:5.1f} Hz".format(self.tone),
+            dcs=self.dcs,
+            dcs_pol=self.dcs_pol.name.replace('_', ' '),
+            usrtone="1600 Hz",
+            rxdgid="RX 00",
+            txdgid="TX 00",
+            pwr=pwr,
+            skip="ON" if self.skip else "OFF",
+            autostep="ON",
+            step="AUTO" if self.step == Step._AUTO else f"{self.step.name[1:].replace('_','.')}KHz",
+            mask="OFF",
+            attn="ON" if self.attn else "OFF",
+            smeter_sql="ON" if self.squelch else "OFF",
+            bell="ON" if self.bell else "OFF",
+            bndw="ON" if self.bandw is Bandw.Narrow else "OFF",
+            clock_shift="ON" if self.clock_shift == ClockShift.B else "OFF",
+            banks=",".join(["ON" if x else "OFF" for x in self.banks]),
+            comment="",
             )
 
     def _parse_name(self, data):
